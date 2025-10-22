45d7b6cb3db9f4172fd7a5438c6c3858ddb2b140
Pim Pieters <pim@pieters.tv>
2025-10-22 16:52:47 +0200

    Add scorer

--- src/score.ts
+++ src/score.ts
@@ -1,4 +1,14 @@
+import { CompletedHand } from "./hand";
 import { Tile } from "./tile";
+import { Riichi } from "./yaku/riichi";
+import { Tanyao } from "./yaku/tanyao";
+import { Tsumo } from "./yaku/tsumo";
+import { Yaku, YakuId } from "./yaku/yaku";
+
+export interface Score {
+  yakus: YakuId[];
+  han: number;
+}
 
 export interface ScoreConfig {
   riichi: boolean;
@@ -7,6 +17,40 @@ export interface ScoreConfig {
   dora: Tile[];
   uradora: Tile[];
   akadora: number;
+  wallCount: number;
 }
 
-export class Scorer {}
+export class Scorer {
+  public readonly config: ScoreConfig;
+  public readonly yakus: Yaku[];
+
+  constructor(config: Partial<ScoreConfig>) {
+    this.config = {
+      riichi: config.riichi ?? false,
+      tsumo: config.tsumo ?? false,
+      dealer: config.dealer ?? false,
+      dora: config.dora ?? [],
+      uradora: config.uradora ?? [],
+      akadora: config.akadora ?? 0,
+      wallCount: config.wallCount ?? 0,
+    };
+
+    this.yakus = [new Tanyao(), new Tsumo(), new Riichi()];
+  }
+
+  public score(hand: CompletedHand): Score {
+    const score: Score = {
+      yakus: [],
+      han: 0,
+    };
+
+    for (const yaku of this.yakus) {
+      if (yaku.check(hand, this.config)) {
+        score.yakus.push(yaku.id);
+        score.han += yaku.han;
+      }
+    }
+
+    return score;
+  }
+}
---
+++ test/score.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "@jest/globals";
+import { Tiles } from "../src/tile";
+import { mockConfig, verifyUnique } from "./mock";
+import { Scorer } from "../src/score";
+import { YakuId } from "../src/yaku/yaku";
+
+describe("basic scoring", () => {
+  test("score tanyao tsumo", () => {
+    // prettier-ignore
+    const tiles = [Tiles.Sou2, Tiles.Sou3, Tiles.Sou4, Tiles.Man4, Tiles.Man4, Tiles.Man4, Tiles.Pin5, Tiles.Pin6, Tiles.Pin7, Tiles.Pin8, Tiles.Pin8, Tiles.Pin8, Tiles.Sou7, Tiles.Sou7];
+    const hand = verifyUnique(tiles);
+    const config = mockConfig();
+    config.tsumo = true;
+    config.riichi = false;
+
+    const scorer = new Scorer(config);
+    const score = scorer.score(hand);
+
+    expect(score.han).toEqual(2);
+    expect(score.yakus.includes(YakuId.Tanyao));
+    expect(score.yakus.includes(YakuId.Tsumo));
+    expect(!score.yakus.includes(YakuId.Riichi));
+  });
+});
