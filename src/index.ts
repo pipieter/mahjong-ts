14e52fc2bd3e1917c6f8029817c4ce47aa158e45
Pim Pieters <pim@pieters.tv>
2025-10-22 17:12:35 +0200

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from "./hand";
+export * from "./score";
+export * from "./tile";
+export * from "./util";
+
+export * from "./yaku/yaku";
+export * from "./yaku/tanyao";
+export * from "./yaku/riichi";
+export * from "./yaku/tsumo";
